51a8f582f5cbc13453a54cab08977b58f05d07b4
Rafael Mendez Asencio <rmendez1972@hotmail.com>
2016-08-30 11:57:08 -0500

    actualizando carpeta vendor

---
+++ vendor/dinesh/barcode/src/config/config.php
@@ -0,0 +1,5 @@
+<?php
+
+return array(
+    'store_path' => public_path("/assets/imagenes/qr_code/"),
+);
---
+++ vendor/thujohn/pdf/src/config/config.php
@@ -0,0 +1,28 @@
+<?php
+return array(
+	'DOMPDF_TEMP_DIR' => sys_get_temp_dir(),
+	'DOMPDF_UNICODE_ENABLED' => true,
+	'DOMPDF_PDF_BACKEND' => 'CPDF',
+	'DOMPDF_DEFAULT_MEDIA_TYPE' => 'screen',
+	'DOMPDF_DEFAULT_PAPER_SIZE' => 'legal',
+	'DOMPDF_DEFAULT_FONT' => 'serif',
+	'DOMPDF_DPI' => 94,
+	'DOMPDF_ENABLE_PHP' => false,
+	'DOMPDF_ENABLE_REMOTE' => true,
+	'DOMPDF_ENABLE_CSS_FLOAT' => true,
+	'DOMPDF_ENABLE_JAVASCRIPT' => true,
+	'DEBUGPNG' => false,
+	'DEBUGKEEPTEMP' => false,
+	'DEBUGCSS' => false,
+	'DEBUG_LAYOUT' => false,
+	'DEBUG_LAYOUT_LINES' => true,
+	'DEBUG_LAYOUT_BLOCKS' => true,
+	'DEBUG_LAYOUT_INLINE' => true,
+	'DOMPDF_FONT_HEIGHT_RATIO' => 1.1,
+	'DEBUG_LAYOUT_PADDINGBOX' => true,
+	'DOMPDF_ENABLE_HTML5PARSER' => false,
+	'DOMPDF_ENABLE_FONTSUBSETTING' => false,
+	'DOMPDF_ADMIN_USERNAME' => 'user',
+	'DOMPDF_ADMIN_PASSWORD' => 'password',
+	'mimedida'  => array(0,0,792.00, 1224.00) //este es el que he agregado
+);
