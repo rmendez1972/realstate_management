7985d9c1963864fb9c083d5df33cefd7ca5e9e6b
Rafael Mendez Asencio <rmendez1972@hotmail.com>
2017-08-04 15:39:02 -0500

    actualizando db

--- public/db_sql/asesores_2015-01-23.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE  `asesores` ADD  `referencia` VARCHAR( 20 ) NOT NULL ,
-ADD  `matricula` VARCHAR( 30 ) NOT NULL ;
--- public/db_sql/asesores_2015-02-09.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `asesores` ADD `localidad` VARCHAR(100) NOT NULL , ADD `fecha_matriculacion` DATE NOT NULL ;
